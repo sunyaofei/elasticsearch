2fa40051ddfc5001c06be52a822b5846e22566db
yaofei.sun <yaofei.sun@qunar.com>
2018-05-29 10:09:50 +0800

    jdk10.sh

---
+++ jdk10.sh
@@ -0,0 +1,6 @@
+JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-10.0.1.jdk/Contents/Home"
+CLASS_PATH=".:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar"
+PATH="$JAVA_HOME/bin:$PATH"
+export JAVA_HOME
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
